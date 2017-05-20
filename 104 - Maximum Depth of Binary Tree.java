66425ff8528b4fbe333a1874a24769de746913a3
derekwang <derekwang1994@yahoo.com>
2017-05-20 21:02:13 +0800

    Created

---
+++ 104 - Maximum Depth of Binary Tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+		return 0;
+
+	Deque<TreeNode> stack = new LinkedList<TreeNode>();
+
+	stack.push(root);
+	int count = 0;
+
+	while (!stack.isEmpty()) {
+		int size = stack.size();
+		while (size-- > 0) {
+			TreeNode cur = stack.pop();
+			if (cur.left != null)
+				stack.addLast(cur.left);
+			if (cur.right != null)
+				stack.addLast(cur.right);
+		}
+		count++;
+
+	}
+  
+	return count;
+    }
+}
