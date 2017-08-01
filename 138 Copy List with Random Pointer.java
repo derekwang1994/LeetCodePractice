d4068d705390d7a4df3c978b687f0851259b8bdb
derekwang <derekwang1994@yahoo.com>
2017-08-01 22:53:03 +0800

    LinkedList, HashMap, ver1, Time O(n), Space O(n)

---
+++ 138 Copy List with Random Pointer.java
@@ -0,0 +1,23 @@
+// A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+// Return a deep copy of the list.
+
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+
+        RandomListNode node = head;
+        while(node!=null){
+            map.put(node, new RandomListNode(node.label));
+            node = node.next;
+        }
+
+        node = head;
+        while(node!=null){
+            map.get(node).next = map.get(node.next);
+            map.get(node).random = map.get(node.random);
+            node = node.next;
+        }
+
+        return map.get(head);
+    }
+}
